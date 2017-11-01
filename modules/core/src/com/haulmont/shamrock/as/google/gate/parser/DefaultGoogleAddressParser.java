63bf046a92e08e770dbb5c95c3e91d159b95b6b5
bozhko <bozhko@haulmont.com>
2017-11-01 19:05:08 +0400

    #14 DWR-494 Support launch of new cities on app/web (try to parse address from formattedAddress)

--- modules/core/src/com/haulmont/shamrock/as/google/gate/parser/DefaultGoogleAddressParser.java
+++ modules/core/src/com/haulmont/shamrock/as/google/gate/parser/DefaultGoogleAddressParser.java
@@ -113,7 +113,7 @@ public class DefaultGoogleAddressParser extends AbstractGoogleAddressParser {
             if (StringUtils.isNotBlank(ctx.city)) {
                 int idx = formattedAddress.toUpperCase().indexOf(", " + StringUtils.upperCase(ctx.city) + ",");
                 if (idx > 0)
-                    address = formattedAddress.substring(idx);
+                    address = formattedAddress.substring(0, idx);
                 else
                     address = formattedAddress;
             }
