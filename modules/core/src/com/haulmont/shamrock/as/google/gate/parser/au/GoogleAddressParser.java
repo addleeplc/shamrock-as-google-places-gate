553e99d5deede91b6a0764968569e2e706f4a803
Nikita Bozhko <bozhko@haulmont.com>
2017-12-07 16:19:09 +0400

    #15 [MULTI-539] Melbourne - Point to Point - Buy Rate is 0

--- modules/core/src/com/haulmont/shamrock/as/google/gate/parser/au/GoogleAddressParser.java
+++ modules/core/src/com/haulmont/shamrock/as/google/gate/parser/au/GoogleAddressParser.java
@@ -10,13 +10,72 @@ import com.haulmont.shamrock.as.google.gate.dto.AddressComponent;
 import com.haulmont.shamrock.as.google.gate.dto.enums.GElement;
 import com.haulmont.shamrock.as.google.gate.parser.DefaultGoogleAddressParser;
 import com.haulmont.shamrock.as.google.gate.parser.Parser;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Parser("AU")
 public class GoogleAddressParser extends DefaultGoogleAddressParser {
+    protected static final Set<String> MELBOURNE_SUB_URBANS = new HashSet<>(
+            Arrays.asList(
+                    "Docklands", 
+                    "Docklands 3008", 
+                    "Port Phillip", 
+                    "Yarra", 
+                    "Banyule", 
+                    "Darebin", 
+                    "Hume", 
+                    "Moonee Valley", 
+                    "Moreland City", 
+                    "Nillumbik", 
+                    "Whittlesea", 
+                    "Boroondara", 
+                    "Knox", 
+                    "Manningham", 
+                    "Maroondah", 
+                    "Whitehorse", 
+                    "Yarra Ranges", 
+                    "Bayside", 
+                    "Cardinia", 
+                    "Casey", 
+                    "Greater Dandenong", 
+                    "Frankston", 
+                    "Glen Eira", 
+                    "Kingston", 
+                    "Monash", 
+                    "Mornington Peninsula", 
+                    "Stonnington", 
+                    "Brimbank", 
+                    "Hobsons Bay", 
+                    "Maribyrnong", 
+                    "Melton", 
+                    "Wyndham"
+            )
+    );
+    
     @Override
     protected String parseCity(Map<String, AddressComponent> components) {
-        return getFirstLong(components, GElement.locality, GElement.political);
+        String city = getFirstLong(components, GElement.locality, GElement.political);
+        if (StringUtils.equalsIgnoreCase(city, "Melbourne"))
+            return city;
+
+        city = getFirstShort(components, GElement.administrative_area_level_2, GElement.political);
+
+        return isMelbourne(city) ? "Melbourne" : getFirstLong(components, GElement.locality, GElement.political);
+    }
+
+    private boolean isMelbourne(String suburb) {
+        if (StringUtils.containsIgnoreCase(suburb, "melbourne"))
+            return true;
+
+        for (String subUrban : MELBOURNE_SUB_URBANS) {
+            if (StringUtils.equalsIgnoreCase(suburb, subUrban) || StringUtils.containsIgnoreCase(suburb, subUrban))
+                return true;
+        }
+
+        return false;
     }
 }
